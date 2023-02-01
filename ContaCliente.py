011ecf096955be3eba60b3ccae208e87ca7e5347
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-01-31 22:10:13 -0300

    Exemplo de Classe Abstrata

--- ContaCliente.py
+++ ContaCliente.py
@@ -1,4 +1,6 @@
-class ContaCliente:
+from abc import ABC, abstractmethod
+
+class ContaCliente(ABC):
     def __init__(self, numero, iof, ir, valor_investido, taxarendimento):
         self.numero = numero
         self.iof = iof
@@ -7,9 +9,9 @@ class ContaCliente:
         self.taxarendimento = taxarendimento
         self.tipo_conta = 'cliente'
 
+    @abstractmethod
     def CalculoRendimento(self):
-        self.valor_investido += (self.valor_investido * self.taxarendimento)
-        self.valor_investido -= (self.valor_investido * (self.iof + self.ir))
+        pass
 
     def Extrato(self):
         print(f"O saldo atual da conta {self.tipo_conta} nº {self.numero} é {self.valor_investido:.2f}")
--- Teste.py
+++ Teste.py
@@ -8,13 +8,16 @@ IR = 0.1
 taxarendimento = 0.05
 
 banco_1 = Banco(2023, "PagBank")
-conta_cliente_1 = ContaCliente(1, IOF, IR, 1000, taxarendimento)
-conta_comum_1 = ContaComum(2, IOF, IR, 2000, taxarendimento)
-conta_remunerada_1 = ContaRemunerada(3, IOF, IR, 2000, taxarendimento)
 
-banco_1.adicionar_conta(conta_cliente_1)
-banco_1.adicionar_conta(conta_comum_1)
-banco_1.adicionar_conta(conta_remunerada_1)
+#* Classe abstrata não pode ser instanciada
+# cc1 = ContaCliente(1, IOF, IR, 1000, taxarendimento)
+
+cc2 = ContaComum(1, IOF, IR, 2000, taxarendimento)
+cr1 = ContaRemunerada(1, IOF, IR, 2000, taxarendimento)
+
+banco_1.adicionar_conta(cc2)
+banco_1.adicionar_conta(cr1)
 
 banco_1.calcular_rendimento_mensal()
+
 banco_1.imprime_saldo_contas()
